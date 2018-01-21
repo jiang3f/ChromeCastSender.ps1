2154660edfd2f8cd9cf4d2971eecbe7db1a77f07
jiang3f <jiang3f@gmail.com>
2018-01-21 01:15:57 -0800

    debug code

--- ChromeCastSender.ps1
+++ ChromeCastSender.ps1
@@ -26,12 +26,16 @@
 
 param (
     #[Parameter(Mandatory=$false)][hashtable]$soa_hash = @{"soa_time_numerator" = 1; "soa_time_denominator" = 1; "soa_duration_denominator" = 1; "soa_duration_numerator" = 1; "outMedia" = "d:\vantage_store\9f6a097b-a131-4a87-8cb7-42c71f0d30ee\";},
-    [URI]$DASH = 'file://D:\vantage_store\6dc8e7bd-261c-40f6-ac64-46e269daf7aa\sourcempeg2_422_pro_ntsc\sourcempeg2_422_pro_ntsc.mpd',
+    #[URI]$DASH = 'file://D:\vantage_store\e5543806-92d6-4048-b6f2-904e1e387d3f\sourcempeg2_422_pro_ntsc\sourcempeg2_422_pro_ntsc.m3u8',
+    #[URI]$DASH = 'file://d:\everyday\20180115\chromecast\ezr.m3u8',
+    [URI]$DASH = 'file://d:\everyday\20180121\chromecast\ezr.m3u8',
     #[URI]$DASH,
-    [string]$ChromecastIpAddr,
+    [string]$ChromecastIpAddr= '192.168.1.8',
     [string]$wwwRoot ='c:\test',
-    [string]$httpServerAddr = '192.168.1.10',
-    [string]$httpServerPort = '8787'
+    [string]$httpServerAddr = '192.168.1.6',
+    #[string]$httpServerAddr = 'commondatastorage.googleapis.com',
+    [string]$httpServerPort = '8000'
+    #[string]$httpServerPort = '80'
 )
 
 $DebugPreference = 'continue'
@@ -209,6 +213,12 @@ $Certificate = $null
 #
 $extName = [System.IO.Path]::GetExtension($DASH.LocalPath)
 $shortName = [System.IO.Path]::GetFileName($DASH.LocalPath)
+
+
+#
+# for test
+#$shortName = 'gtv-videos-bucket/sample/BigBuckBunny.mp4'
+
 if($extName -eq ".mpd" -or $extName -eq ".ismc" -or $extName -eq ".m3u8")
 {
     $sourcePath = Split-Path -Path $($DASH.LocalPath) 
@@ -370,11 +380,11 @@ if($extName -eq '.mp4')
 {
     $codec = "video/mp4"
 }
-elseif($exName -eq '.mpd')
+elseif($extName -eq '.mpd')
 {
     $codec = "application/dash+xml"
 }
-elseif($exName -eq '.m3u8')
+elseif($extName -eq '.m3u8')
 {
     $codec = "application/x-mpegurl"
 }
@@ -383,7 +393,8 @@ elseif($extName -eq '.ismc')
     $codec = "application/vnd.ms-sstr+xml"
 }
 
-$data = '{"type":"LOAD","requestId":46479002,"sessionId":"' + $sessionId + '","media":{"contentId":"http://' + $httpServerAddr + ':' + $httpServerPort + '/' + $shortName + '","streamType":"buffered","contentType":"' + $codec + '"},"autoplay":true,"currentTime":0,"customData":{"payload":{"title:":"test_file","thumb":"images/BigBuckBunny.jpg"}}}'
+#$data = '{"type":"LOAD","requestId":46479002,"sessionId":"' + $sessionId + '","media":{"contentId":"http://' + $httpServerAddr + ':' + $httpServerPort + '/' + $shortName + '","streamType":"buffered","contentType":"' + $codec + '"},"autoplay":true,"currentTime":0,"customData":{"payload":{"title:":"test_file","thumb":"images/BigBuckBunny.jpg"}}}'
+$data = '{"type":"LOAD","requestId":46479002,"sessionId":"' + $sessionId + '","media":{"contentId":"http://' + $httpServerAddr + ':' + $httpServerPort + '/' + $shortName + '","streamType":"buffered","contentType":"' + $codec + '","metadata":[{"metadataType":0}]},"autoplay":true,"currentTime":0,"customData":{"payload":{"title:":"test_file","thumb":"images/BigBuckBunny.jpg"}}}'
 
 $chrome_namespace = "urn:x-cast:com.google.cast.media"
 $msg = CreateRequest $data $chrome_namespace ([ref]$len) $transportId
